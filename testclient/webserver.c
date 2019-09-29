f95c23a3bf927e4fe28352a2b854107df4f76f95
qilihui <1499910521@qq.com>
2019-09-29 23:43:34 +0000

    2019.09.29 23:43

--- testclient/webserver.c
+++ testclient/webserver.c
@@ -1,210 +1,412 @@
+/*
+* gcc -g -o http-server http-server.c -levent
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <evhttp.h>
-#include <event.h>
 #include <string.h>
-#include "event2/http.h"
-#include "event2/event.h"
-#include "event2/buffer.h"
-#include "event2/bufferevent.h"
-#include "event2/bufferevent_compat.h"
-#include "event2/http_struct.h"
-#include "event2/http_compat.h"
-#include "event2/util.h"
-#include "event2/listener.h"
 
-#define BUF_MAX 1024*16
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
 
-//解析post请求数据
-void get_post_message(char *buf, struct evhttp_request *req)
+#include <event2/event.h>
+#include <event2/http.h>
+#include <event2/buffer.h>
+#include <event2/util.h>
+#include <event2/keyvalq_struct.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+char uri_root[512];
+
+static const struct table_entry {
+    const char *extension;
+    const char *content_type;
+} content_type_table[] = {
+    { "txt", "text/plain" }, 
+    { "c", "text/plain" }, 
+    { "h", "text/plain" }, 
+    { "html", "text/html" }, 
+    { "htm", "text/html" },
+    { "css", "text/css" },
+    { "gif", "image/gif" }, 
+    { "jpg", "image/jpg" }, 
+    { "jpeg", "image/jpeg" }, 
+    { "png", "image/png" }, 
+    { "pdf", "application/pdf" }, 
+    { "ps", "application/postscript" }, 
+    { NULL, NULL },
+};
+
+/* Try to guess a good content-type for 'path' */
+const char* guess_content_type(const char *path)
 {
-    size_t post_size = 0;
+    const char *last_period, *extension;
+    const struct table_entry *ent;
+    last_period = strrchr(path, '.');
+    if (!last_period || strchr(last_period, '/'))
+    {
+        goto not_found;
+    }
 
-    post_size = evbuffer_get_length(req->input_buffer);//获取数据长度
-    printf("====line:%d,post len:%d\n",__LINE__,post_size);
-    if (post_size <= 0)
+    extension = last_period + 1;
+    for (ent = &content_type_table[0]; ent->extension; ++ent)
     {
-        printf("====line:%d,post msg is empty!\n",__LINE__);
-        return;
+        if (!evutil_ascii_strcasecmp(ent->extension, extension))
+        {
+            return ent->content_type;
+        }
     }
-    else
-    {
-        size_t copy_len = post_size > BUF_MAX ? BUF_MAX : post_size;
-        printf("====line:%d,post len:%d, copy_len:%d\n",__LINE__,post_size,copy_len);
-        memcpy(buf, evbuffer_pullup(req->input_buffer,-1), copy_len);
-        buf[post_size] = '\0';
-        printf("====line:%d,post msg:%s\n",__LINE__,buf);
-    }
+
+not_found:
+    return "application/misc";
 }
 
-//解析http头，主要用于get请求时解析uri和请求参数
-char *find_http_header(struct evhttp_request *req,struct evkeyvalq *params,const char *query_char)
+/* Callbase used for the /dump URI, and for every non-get request:
+** dumps all information to stdout and gives base a trivial 200 ok */
+void dump_request_cb(struct evhttp_request *req, void *arg)
 {
-    if(req == NULL || params == NULL || query_char == NULL)
+    const char *cmdtype;
+    struct evkeyvalq *headers;
+    struct evkeyval *header;
+    struct evbuffer *buf;
+
+    switch (evhttp_request_get_command(req))
     {
-        printf("====line:%d,%s\n",__LINE__,"input params is null.");
-        return NULL;
+    case EVHTTP_REQ_GET:
+        cmdtype = "GET";
+        break;
+    case EVHTTP_REQ_POST:
+        cmdtype = "POST";
+        break;
+    case EVHTTP_REQ_HEAD:
+        cmdtype = "HEAD";
+        break;
+    case EVHTTP_REQ_PUT:
+        cmdtype = "PUT";
+        break;
+    case EVHTTP_REQ_DELETE:
+        cmdtype = "DELETE";
+        break;
+    case EVHTTP_REQ_OPTIONS:
+        cmdtype = "OPTIONS";
+        break;
+    case EVHTTP_REQ_TRACE:
+        cmdtype = "TRACE";
+        break;
+    case EVHTTP_REQ_CONNECT:
+        break;
+    case EVHTTP_REQ_PATCH:
+        cmdtype = "PATCH";
+        break;
+    default:
+        cmdtype = "unknown";
+        break;
     }
 
+    printf("Received a %s request for %s\nHeader:\n", cmdtype, evhttp_request_get_uri(req));
+
+    headers = evhttp_request_get_input_headers(req);
+    for (header = headers->tqh_first; header; header = header->next.tqe_next)
+    {
+        printf(" %s: %s\n", header->key, header->value);
+    }
+
+    buf = evhttp_request_get_input_buffer(req);
+    puts("Input data: <<<<");
+    while (evbuffer_get_length(buf))
+    {
+        int n;
+        char cbuf[128];
+        n = evbuffer_remove(buf, cbuf, sizeof(cbuf));
+        if (n > 0)
+        {
+            (void)fwrite(cbuf, 1, n, stdout);
+        }
+    }
+    puts(">>>");
+
+    evhttp_send_reply(req, 200, "ok", NULL);
+}
+
+/* This callback gets invoked when we get and http request than doesn't match 
+* any other callback. Like any evhttp server callback, it has a simple job:
+* it must eventually call evhttp_send_error() or evhttp_send_reply(). 
+*/
+void send_document_cb(struct evhttp_request *req, void *arg)
+{
+    struct evbuffer *evb = NULL;
+    const char *docroot = arg;
+    const char *uri = evhttp_request_get_uri(req);
     struct evhttp_uri *decoded = NULL;
-    char *query = NULL; 
-    char *query_result = NULL;
     const char *path;
-    const char *uri = evhttp_request_get_uri(req);//获取请求uri
+    char *decoded_path;
+    char *whole_path = NULL;
+    size_t len;
+    int fd = -1;
+    struct stat st;
 
-    if(uri == NULL)
+    if (evhttp_request_get_command(req) != EVHTTP_REQ_GET)
     {
-        printf("====line:%d,evhttp_request_get_uri return null\n",__LINE__);
-        return NULL;
-    }
-    else
-    {
-        printf("====line:%d,Got a GET request for <%s>\n",__LINE__,uri);
+        dump_request_cb(req, arg);
+        return;
     }
 
-    //解码uri
+    printf("Got a GET request for <%s>\n", uri);
+
+    /* Decode the URI */
     decoded = evhttp_uri_parse(uri);
-    if (!decoded) 
+    if (!decoded)
     {
-        printf("====line:%d,It's not a good URI. Sending BADREQUEST\n",__LINE__);
+        printf("It's not a good URI, Sneding BADREQUEST\n");
         evhttp_send_error(req, HTTP_BADREQUEST, 0);
         return;
     }
 
-    //获取uri中的path部分
+    /* Let's see what path the user asked for. */
     path = evhttp_uri_get_path(decoded);
-    if (path == NULL) 
+    if (!path)
     {
         path = "/";
     }
+
+    /* We need to decode it, to see what path the user really wanted */
+    decoded_path = evhttp_uridecode(path, 0, NULL);
+    if (decoded_path == NULL)
+    {
+        goto err;
+    }
+
+    /* Don't allow any ".."'s in the path, to avoid exposing stuff outside 
+    * of the docroot. This test is both overzealous and underzealous:
+    * it forbids aceptable paths like "/this/one..here", but it doesn't
+    * do anything to prevent symlink following. */
+    if (strstr(decoded_path, ".."))
+    {
+        goto err;
+    }
+
+    len = strlen(decoded_path) + strlen(docroot) + 2;
+    if (!(whole_path = malloc(len)))
+    {
+        perror("malloc");
+        goto err;
+    }
+    evutil_snprintf(whole_path, len, "%s/%s", docroot, decoded_path);
+
+    if (stat(whole_path, &st) < 0)
+    {
+        goto err;
+    }
+
+    /* This holds the content we're sending */
+    evb = evbuffer_new();
+
+    if (S_ISDIR(st.st_mode))
+    {
+        /* If it's a directory, read the comments and make a little index page */
+        DIR *d;
+        struct dirent *ent;
+        const char *trailing_slash = "";
+
+        if (!strlen(path) || path[strlen(path) - 1] != '/')
+        {
+            trailing_slash = "/";
+        }
+
+        if (!(d = opendir(whole_path)))
+        {
+            goto err;
+        }
+
+        evbuffer_add_printf(evb, 
+            "<!DOCTYPE html>\n"
+            "<html>\n  "
+            "  <head>\n"
+            "    <meta charset='utf-8'>\n"
+            "    <title>%s</title>\n"
+            "    <base href='%s%s'>\n"
+            "  </head>\n"
+            "  <body>\n"
+            "    <h1>%s</h1>\n"
+            "    <ul>\n", 
+            decoded_path, /* xxx html-escape this. */
+            path, /* xxx html-escape this? */
+            trailing_slash, 
+            decoded_path /* xx html-escape this */
+            );
+
+        while ((ent = readdir(d)))
+        {
+            const char *name = ent->d_name;
+            evbuffer_add_printf(evb,
+                "    <li><a href=\"%s\">%s</a>\n", name, name);
+        }
+
+        evbuffer_add_printf(evb, "</ul></body></html>\n");
+        closedir(d);
+        evhttp_add_header(evhttp_request_get_output_headers(req),
+            "Content-Type", "text/html");
+    }
     else
     {
-        printf("====line:%d,path is:%s\n",__LINE__,path);
+        /* Otherwise it's a file; and it to the buffer to get send via sendfile */
+        const char *type = guess_content_type(decoded_path);
+        if ((fd = open(whole_path, O_RDONLY)) < 0)
+        {
+            perror("open");
+            goto err;
+        }
+
+        if (fstat(fd, &st) < 0)
+        {
+            /* Make sure the length still matches, now that we opened the file :/ */
+            perror("fstat");
+            goto err;
+        }
+        evhttp_add_header(evhttp_request_get_output_headers(req),
+            "Content-Type", type);
+        evbuffer_add_file(evb, fd, 0, st.st_size);
     }
 
-    //获取uri中的参数部分
-    query = (char*)evhttp_uri_get_query(decoded);
-    if(query == NULL)
+    evhttp_send_reply(req, 200, "OK", evb);
+    goto done;
+
+err:
+    evhttp_send_error(req, 404, "Document was not found");
+    if (fd >= 0)
     {
-        printf("====line:%d,evhttp_uri_get_query return null\n",__LINE__);
-        return NULL;
+        close(fd);
     }
 
-    //查询指定参数的值
-    evhttp_parse_query_str(query, params);          
-    query_result = (char*)evhttp_find_header(params, query_char);
+done:
+    if (decoded)
+    {
+        evhttp_uri_free(decoded);
+    }
 
-    return query_result;
+    if (decoded_path)
+    {
+        free(decoded_path);
+    }
+
+    if (whole_path)
+    {
+        free(whole_path);
+    }
+
+    if (evb)
+    {
+        evbuffer_free(evb);
+    }
 }
 
-//处理get请求
-void http_handler_testget_msg(struct evhttp_request *req,void *arg)
+void syntax(void)
 {
-    if(req == NULL)
-    {
-        printf("====line:%d,%s\n",__LINE__,"input param req is null.");
-        return;
-    }
-
-    char *sign = NULL;
-    char *data = NULL;
-    struct evkeyvalq sign_params = {0};
-    sign = find_http_header(req,&sign_params,"sign");//获取get请求uri中的sign参数
-    if(sign == NULL)
-    {
-        printf("====line:%d,%s\n",__LINE__,"request uri no param sign.");
-    }
-    else
-    {
-        printf("====line:%d,get request param: sign=[%s]\n",__LINE__,sign);
-    }
-
-    data = find_http_header(req,&sign_params,"data");//获取get请求uri中的data参数
-    if(data == NULL)
-    {
-        printf("====line:%d,%s\n",__LINE__,"request uri no param data.");
-    }
-    else
-    {
-        printf("====line:%d,get request param: data=[%s]\n",__LINE__,data);
-    }
-    printf("\n");
-
-    //回响应
-    struct evbuffer *retbuff = NULL;
-    retbuff = evbuffer_new();
-    if(retbuff == NULL)
-    {
-        printf("====line:%d,%s\n",__LINE__,"retbuff is null.");
-        return;
-    }
-    evbuffer_add_printf(retbuff,"Receive get request,Thamks for the request!");
-    evhttp_send_reply(req,HTTP_OK,"Client",retbuff);
-    evbuffer_free(retbuff);
+    fprintf(stdout, "Syntax: http-server <docroot>\n");
 }
 
-//处理post请求
-void http_handler_testpost_msg(struct evhttp_request *req,void *arg)
+int main(int argc, char **argv)
 {
-    if(req == NULL)
-    {
-        printf("====line:%d,%s\n",__LINE__,"input param req is null.");
-        return;
-    }
+    struct event_base *base;
+    struct evhttp *http;
+    struct evhttp_bound_socket *handle;
+    int port = 8080;
 
-    char buf[BUF_MAX] = {0};
-    get_post_message(buf, req);//获取请求数据，一般是json格式的数据
-    if(buf == NULL)
+    if (signal(SIGPIPE, SIG_IGN) == SIG_ERR)
     {
-        printf("====line:%d,%s\n",__LINE__,"get_post_message return null.");
-        return;
-    }
-    else
-    {
-        //可以使用json库解析需要的数据
-        printf("====line:%d,request data:%s",__LINE__,buf);
-    }
-
-    //回响应
-    struct evbuffer *retbuff = NULL;
-    retbuff = evbuffer_new();
-    if(retbuff == NULL)
-    {
-        printf("====line:%d,%s\n",__LINE__,"retbuff is null.");
-        return;
-    }
-    evbuffer_add_printf(retbuff,"Receive post request,Thamks for the request!");
-    evhttp_send_reply(req,HTTP_OK,"Client",retbuff);
-    evbuffer_free(retbuff);
-}
-
-int main()
-{
-    struct evhttp *http_server = NULL;
-    short http_port = 8081;
-    char *http_addr = "0.0.0.0";
-
-    //初始化
-    event_init();
-    //启动http服务端
-    http_server = evhttp_start(http_addr,http_port);
-    if(http_server == NULL)
-    {
-        printf("====line:%d,%s\n",__LINE__,"http server start failed.");
+        printf("signal error, errno[%d], error[%s]", errno, strerror(errno));
         return -1;
     }
 
-    //设置请求超时时间(s)
-    evhttp_set_timeout(http_server,5);
-    //设置事件处理函数，evhttp_set_cb针对每一个事件(请求)注册一个处理函数，
-    //区别于evhttp_set_gencb函数，是对所有请求设置一个统一的处理函数
-    evhttp_set_cb(http_server,"/me/testpost",http_handler_testpost_msg,NULL);
-    evhttp_set_cb(http_server,"/me/testget",http_handler_testget_msg,NULL);
+    if (argc < 2)
+    {
+        syntax();
+        return -1;
+    }
 
-    //循环监听
-    event_dispatch();
-    //实际上不会释放，代码不会运行到这一步
-    evhttp_free(http_server);
+    base = event_base_new();
+    if (!base)
+    {
+        printf("Couldn't create an event_base:exiting\n");
+        return -1;
+    }
+
+    /* Create a new http oject to handle request */
+    http = evhttp_new(base);
+    if (!http)
+    {
+        printf("Couldn't create evhttp.Exiting\n");
+        return -1;
+    }
+
+    /* The /dump URI will dump all requests to stdout and say 200 ok */
+    evhttp_set_cb(http, "/dump", dump_request_cb, NULL);
+
+    /* We want to accept arbitrary requests, so we need to set a "generic" cb
+    * We can also add callbacks for specific paths */
+    evhttp_set_gencb(http, send_document_cb, argv[1]);
+
+    /* Now we teel the evhttp what port to listen on */
+    handle = evhttp_bind_socket_with_handle(http, "0.0.0.0", port);
+    if (!handle)
+    {
+        printf("Couldn't bind to port[%d], exiting\n", port);
+        return -1;
+    }
+
+    {
+        /* Extract and display the address we're listening on. */
+        struct sockaddr_storage ss;
+        evutil_socket_t fd;
+        ev_socklen_t socklen = sizeof(ss);
+        char addrbuf[128];
+        void *inaddr;
+        const char *addr;
+        int got_port = -1;
+        fd = evhttp_bound_socket_get_fd(handle);
+        memset(&ss, 0, sizeof(ss));
+        if (getsockname(fd, (struct sockaddr *)&ss, &socklen))
+        {
+            perror("getsockname() failed");
+            return -1;
+        }
+
+        if (ss.ss_family == AF_INET)
+        {
+            got_port = ntohs(((struct sockaddr_in*)&ss)->sin_port);
+            inaddr = &((struct sockaddr_in*)&ss)->sin_addr;
+        }
+        else if (ss.ss_family == AF_INET6)
+        {
+            got_port = ntohs(((struct sockaddr_in6*)&ss)->sin6_port);
+            inaddr = &((struct sockaddr_in6*)&ss)->sin6_addr;
+        }
+        else
+        {
+            printf("Weird address family\n");
+            return 1;
+        }
+
+        addr = evutil_inet_ntop(ss.ss_family, inaddr, addrbuf, sizeof(addrbuf));
+        if (addr)
+        {
+            printf("Listening on %s:%d\n", addr, got_port);
+            evutil_snprintf(uri_root, sizeof(uri_root), "http://%s:%d", addr, got_port);
+        }
+        else
+        {
+            printf("evutil_inet_ntop failed\n");
+            return -1;
+        }
+    }
+    event_base_dispatch(base);
 
     return 0;
 }
